8487adc14f43718147fafcc99559114e93f566df
varunkm11 <117428645+varunkm11@users.noreply.github.com>
2026-05-23 14:17:59 +0530

    update

--- services/gemini_chat.py
+++ services/gemini_chat.py
@@ -1,13 +1,16 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 
 def get_chat_model():
+
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-pro",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0.3
     )
--- services/rag_pipeline.py
+++ services/rag_pipeline.py
@@ -1,21 +1,15 @@
 from langchain.chains import RetrievalQA
-from services.qdrant_service import get_qdrant_vectorstore
 from services.gemini_chat import get_chat_model
 
 
-def get_qa_chain():
-    vectorstore = get_qdrant_vectorstore()
-
-    retriever = vectorstore.as_retriever(
-        search_kwargs={"k": 4}
-    )
+def get_qa_chain(retriever):
 
     llm = get_chat_model()
 
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
-        return_source_documents=True
+        chain_type="stuff"
     )
 
     return qa_chain
