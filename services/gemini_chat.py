d8ddf093fef3c353df4dcf56de6b43f2e5eb9a0c
varunkm11 <117428645+varunkm11@users.noreply.github.com>
2026-05-23 14:18:09 +0530

    Revert "update"
    This reverts commit 8487adc14f43718147fafcc99559114e93f566df.

--- services/gemini_chat.py
+++ services/gemini_chat.py
@@ -1,16 +1,13 @@
-import os
-
-from dotenv import load_dotenv
-
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 
 def get_chat_model():
-
     llm = ChatGoogleGenerativeAI(
-        model="gemini-pro",
+        model="gemini-2.5-flash",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0.3
     )
--- services/rag_pipeline.py
+++ services/rag_pipeline.py
@@ -1,15 +1,21 @@
 from langchain.chains import RetrievalQA
+from services.qdrant_service import get_qdrant_vectorstore
 from services.gemini_chat import get_chat_model
 
 
-def get_qa_chain(retriever):
+def get_qa_chain():
+    vectorstore = get_qdrant_vectorstore()
+
+    retriever = vectorstore.as_retriever(
+        search_kwargs={"k": 4}
+    )
 
     llm = get_chat_model()
 
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
-        chain_type="stuff"
+        return_source_documents=True
     )
 
     return qa_chain
